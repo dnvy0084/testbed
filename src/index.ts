2de1fff91fc3f4e7f3bc5487cb9c95fb03f9f78c
jinhoon.kim <jinhoon.kim>
2019-11-02 13:54:16 +0900

    :heavy_plus_sign: tsc, tsconfig 추가

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import A from "./test/A";
+
+let t: string = "";
+
+t = "hello, world! @";
+
+console.log(t);
+
+const a = new A();
+
+a.test();
---
+++ src/test/A.ts
@@ -0,0 +1,9 @@
+export default class A {
+  constructor() {
+    console.log("create A");
+  }
+
+  public test(): void {
+    console.log("call A.test");
+  }
+}
